041b8be6e0e8c375c8913061acf23357f39d5d60
Armel Chiza <armelchiza@gmail.com>
2017-07-04 22:48:27 +0000

    couple of functions left

--- workshop.js
+++ workshop.js
@@ -8,45 +8,106 @@ function forEach(callback, theArray) {
     ...
   }
   */
+  // theArray.forEach(callback(element));
+  
+  for(i=0; i<theArray.length; i++){
+    callback(theArray[i]);
+  }
 }
 
-// COMMENT
-
 function map(mappingFunction, theArray) {
-
+  var newArray = [];
+  for(i=0; i<theArray.length; i++){
+    newArray.push(mappingFunction(theArray[i]));
+  }
+  return newArray;
 }
 
 function filter(predicate, theArray) {
-
+  var newArray = [];
+  var trash = [];
+  for(i=0; i<theArray.length; i++){
+    predicate(theArray[i])?newArray.push(theArray[i]):trash.push(theArray[i]);
+  }
+  return newArray;
 }
 
 function every(predicate, theArray) {
-
+  for (i=0; i<theArray.length; i++){
+    if (!predicate(theArray[i])){
+      return false;
+    }
+  }
+  return (true);
 }
 
 function some(predicate, theArray) {
-
+  for (i=0; i<theArray.length; i++){
+    if (predicate(theArray[i])){
+      return true;
+    }
+  }
+  return (false);
 }
 
 function indexOf(item, theArray) {
-
+  for (i=0; i<theArray.length; i++){
+    if (item === theArray[i]){
+      return i;
+    }
+  }
+  return -1;
 }
 
 function findIndex(predicate, theArray) {
-
+    for (i=0; i<theArray.length; i++){
+    if (predicate( theArray[i])){
+      return i;
+    }
+  }
+  return -1;
 }
 
 function first(n, theArray) {
-
+  newArray = [];
+  number = 0;
+  theArray == undefined ? newArray = n[0] : number=n;
+  for (i=0; i<number; i++){
+    i<theArray.length? newArray.push(theArray[i]) : number=0; 
+  }
+  return(newArray);
 }
+
 
 function last(n, theArray) {
-
+  newArray = [];
+  newNewArray = [];
+  number = 0;
+  theArray == undefined ? newArray = n[n.length-1] : number=n;
+  for (i=0; i<number; i++){
+    i<theArray.length? newArray.push(theArray[theArray.length - 1 - i]) : number=0; 
+  }
+  if (newArray.length > 1) {
+    for (i=0; i<newArray.length; i++){
+      newNewArray.push(newArray[newArray.length-1-i]);
+    }
+    newArray = newNewArray;
+  }
+  return(newArray);
 }
 
-function pluck(property, arrayOfObjects) {
-
+function pluck(property, arrayOfObjects) { // working on this
+  theArray = [];
+  
+  forEach(function(){
+    theArray.push();
+  }, arrayOfObjects)
+  
+  forEach(theArray.push(this.property), arrayOfObjects);
+  return(theArray);
+  
 }
+console.log(pluck('id'), {id: "pet"});
 
 function flatten(theArray) {
 
